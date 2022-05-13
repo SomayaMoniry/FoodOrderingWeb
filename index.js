f83c389fdbc8d76af41f101f21d029564d790636
SomayaMoniry <somayehmoniry23@gmail.com>
2022-05-13 15:00:07 +0430

    adding javaScript and making work the functionality of menu & close buttons and getting the current date

---
+++ index.js
@@ -0,0 +1,17 @@
+"strict mode";
+console.log("hello");
+
+// current date
+const year = document.querySelector(".year");
+const currentDate = new Date().getFullYear();
+year.textContent = currentDate;
+
+// adding functionality of menu and close buttons
+
+const header = document.querySelector(".header");
+
+const btnNav = document.querySelector(".btn-mobile-nav");
+
+btnNav.addEventListener("click", function () {
+  header.classList.toggle("show-nav");
+});
